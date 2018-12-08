9f1854de93a65ca16cde3896901459aa902e4fbc
barper <barper@post.bgu.ac.il>
2018-12-08 08:57:53 +0200

    messageBusUpdate

--- assignmnet2-project/src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ assignmnet2-project/src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -2,6 +2,15 @@ package bgu.spl.mics;
 
 import bgu.spl.mics.application.passiveObjects.Inventory;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicReference;
+
+import bgu.spl.mics.MicroService;
+
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
@@ -11,21 +20,36 @@ public class MessageBusImpl implements MessageBus {
 
 	//@INV: the initialization of the singleton successfully completed
 
+	//hash table with message type key to point on queue of subscribed microServices
+	private ConcurrentHashMap<Class<? extends Message>, ConcurrentLinkedQueue<MicroService>> _messageTypeHT;
+	//hash table with micro service type key to point on queue of micro Services's aimed messages
+	private ConcurrentHashMap<MicroService, ConcurrentLinkedQueue<Class<? extends Message>>> _microServiceTypeHT;
+	//hash table which associates between event to its Future object
+	private ConcurrentHashMap<Class<? extends Event>, Future> _eventToFuture;
+	//lock object
+	private Object _lockEvent;
+	private Object _lockBrod;
+
+
 	/**
-	 * the followings are methods for implementation of the trhread safe singleton
+	 * the followings are methods for implementation of the thread safe singleton
 	 * the method used for the thread safe which is called  "Initialization-on-demand holder (design pattern) idiom"
-	 * this thread safety is guarenteed by the jvm initialization classes
-	 * */
+	 * this thread safety is guaranteed by the jvm initialization classes
+	 */
 
 	//private constructor
-	private MessageBusImpl(){
-		//TODO: Implement this
+	private MessageBusImpl() {
+		//init the hashTables
+		_messageTypeHT = new ConcurrentHashMap<Class<? extends Message>, ConcurrentLinkedQueue<MicroService>>();
+		_microServiceTypeHT = new ConcurrentHashMap<MicroService, ConcurrentLinkedQueue<Class<? extends Message>>>();
+		_eventToFuture = new ConcurrentHashMap<Class<? extends Event>, Future>();
+		_lockEvent = new Object();
+		_lockBrod = new Object();
 	}
 
 	private static class Holder {
 		private static final MessageBusImpl INSTANCE = new MessageBusImpl();
 	}
-
 
 
 	//Retrieves the single instance of this class.
@@ -38,29 +62,43 @@ public class MessageBusImpl implements MessageBus {
 	//@POST:
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// TODO Auto-generated method stub
+
+
+		//get the relevant queue to subscribe (insert the micro service as subscriber)
+		synchronized (_lockEvent) {
+			//init the queue in case the type has no subscribers
+			subscribeMicroService(type, m);
+
+		}
 
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		// TODO Auto-generated method stub
+		synchronized (_lockBrod) {
+			//init the queue in case the type has no subscribers
+			subscribeMicroService(type, m);
 
+		}
 	}
 
 	@Override
 	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
-
+		// TODO
+		//resolve the result that is associated with the received event
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
+		// synchronized ()
+		ConcurrentLinkedQueue<MicroService> q = _messageTypeHT.get(b);
+		for(MicroService m:q){
+			_microServiceTypeHT.get(m).add(b.getClass());   //insert the brodcast message to all subscribers
+		}
 
 	}
 
-	
+
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
 		// TODO Auto-generated method stub
@@ -69,13 +107,13 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public void register(MicroService m) {
-		// TODO Auto-generated method stub
 
 	}
 
+
+	///////////////////////////////not finished!! verify keys insertion to table////////////////////////////////////
 	@Override
 	public void unregister(MicroService m) {
-		// TODO Auto-generated method stub
 
 	}
 
@@ -85,6 +123,9 @@ public class MessageBusImpl implements MessageBus {
 		return null;
 	}
 
-	
+	private void subscribeMicroService(Class<? extends Message> type, MicroService m) {
+		_messageTypeHT.putIfAbsent(type, new ConcurrentLinkedQueue<MicroService>());
+		_messageTypeHT.get(type).add(m);    //add m as subscriber to the type of message
+	}
 
 }
