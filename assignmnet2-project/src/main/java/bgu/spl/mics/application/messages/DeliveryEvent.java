15a77eccea191fd40358774723e860caf1619384
MoranNeptune <neptune@post.bgu.ac.il>
2018-12-11 12:12:31 +0200

    error moran as usual

--- assignmnet2-project/src/main/java/bgu/spl/mics/application/messages/DeliveryEvent.java
+++ assignmnet2-project/src/main/java/bgu/spl/mics/application/messages/DeliveryEvent.java
@@ -2,15 +2,18 @@ package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Event;
 import bgu.spl.mics.application.passiveObjects.OrderReceipt;
 
+/**
+ * An event that is sent when the sells men of the store wishes to deliver the book that was bought
+ */
 public class DeliveryEvent implements Event {
-    private OrderReceipt orderReceipt;
-    private String address;
+    private OrderReceipt receipt;
     private int distance;
+    private String address;
 
-    public DeliveryEvent(OrderReceipt orderReceipt, String address, int distance) {
-        this.orderReceipt = orderReceipt;
-        this.address = address;
+    public DeliveryEvent(OrderReceipt receipt, int distance, String address) {
+        this.receipt = receipt;
         this.distance = distance;
+        this.address = address;
     }
 
     public int getDistance() {
@@ -18,7 +21,7 @@ public class DeliveryEvent implements Event {
     }
 
     public OrderReceipt getOrderReceipt() {
-        return orderReceipt;
+        return receipt;
     }
 
     public String getAddress() {
--- assignmnet2-project/src/main/java/bgu/spl/mics/application/services/TimeService.java
+++ assignmnet2-project/src/main/java/bgu/spl/mics/application/services/TimeService.java
@@ -4,13 +4,16 @@ import bgu.spl.mics.MicroService;
 import bgu.spl.mics.application.messages.CurrTickEvent;
 import bgu.spl.mics.application.messages.TerminateBroadcast;
 import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.passiveObjects.Inventory;
+import bgu.spl.mics.application.passiveObjects.MoneyRegister;
+import bgu.spl.mics.application.passiveObjects.ResourcesHolder;
 
 import javax.swing.*;
 
 /**
  * TimeService is the global system timer There is only one instance of this micro-service.
  * It keeps track of the amount of ticks passed since initialization and notifies
- * all other micro-services about the current time tick using {@link Tick Broadcast}.
+ * all other micro-services about the current time tick using {@link //Tick Broadcast}.
  * This class may not hold references for objects which it is not responsible for:
  * {@link ResourcesHolder}, {@link MoneyRegister}, {@link Inventory}.
  * 
@@ -24,7 +27,7 @@ public class TimeService extends MicroService{
 	int duration;
 	int currTick;
 	public TimeService(int speed, int duration) {
-		super("timer");
+		super("TimeService");
 		currTick=1;
 		this.speed=speed;
 		this.duration=duration;
@@ -32,35 +35,31 @@ public class TimeService extends MicroService{
 
 	@Override
 	protected void initialize() {
+		// when TerminateBroadcast is received then the TimeService should be terminated
+		subscribeBroadcast(TerminateBroadcast.class,terminateBroadcast -> {
+			this.terminate();
+		});
+
+		// when CurrTickEvent is received then the TimeService gets the current tick
+		subscribeEvent(CurrTickEvent.class,currTickEvent -> {
+			complete(currTickEvent,currTick);
+		});
+
 		// Defining the timer activation
 		timer = new Timer(speed,e ->{
 			currTick++;
 			///////////////System.out.println("The current tick is : "+currTick);
 			if(currTick==duration) {// if the duration time is over
-				// timeService tells messageBus to tell all of the services that subscribed to receive
-				// messages from time service that the current time is currTick
-				this.sendBroadcast(new TerminateBroadcast(currTick)); // sends the current tick to the TickBroadCast
+				// timeService tells messageBus to tell all of the services that subscribed to him that
+				// terminates itself
+				this.sendBroadcast(new TerminateBroadcast(currTick));
 				timer.stop();// the duration is over so the timer needs to stop
 			}
 			else // if the duration time is not over
-				sendBroadcast(new TickBroadcast(currTick)); // sends the current tick to the TickBroadCast
-			subBroadcast();
-			subEvent();
+				sendBroadcast(new TickBroadcast(currTick));
 
 			////////////////System.out.println("Services: "+this.getName()+" is terminated");
 			timer.start();
-		});
-	}
-
-	private void subBroadcast(){
-		subscribeBroadcast(TerminateBroadcast.class,c -> {
-			this.terminate();
-		});
-	}
-
-	private void subEvent(){
-		subscribeEvent(CurrTickEvent.class,c -> {
-			complete(c,currTick);
 		});
 	}
 }
